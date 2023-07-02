7a57f5d431ba22eed75f266b1a9b05b86fc81300
Anirudh Oppiliappan <x@icyphox.sh>
2023-07-02 09:31:29 +0300

    Bump User-Agent

--- reader/fetch.go
+++ reader/fetch.go
@@ -50,7 +50,7 @@ func Fetch(url string) (Response, error) {
 			return Response{}, fmt.Errorf("http error: %w\n", err)
 		}
 
-		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36")
+		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
 		resp, err := client.Do(req)
 		if err != nil {
 			return Response{}, fmt.Errorf("http client error: %w\n", err)
